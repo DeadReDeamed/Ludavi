7d00c71b7d02c42f7358dc264ac90479be7c73d9
Luca <Luca.1999@hotmail.com>
2021-10-04 16:46:00 +0200

    Added server logic

---
+++ Ludavi/Ludavi/ServerClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ServerClient
+    {
+        public string IP { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public NetworkStream Stream { get; private set; }
+        public TcpClient client { get; private set; }
+
+        public ServerClient(string IP, string UserName, string Password, TcpClient client)
+        {
+            this.IP = IP;
+            this.UserName = UserName;
+            this.Password = Password;
+            this.client = client;
+            this.Stream = client.GetStream();
+        }
+    }
+}
---
+++ Ludavi/Ludavi/ServerLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ServerLogic
+    {
+        private static Dictionary<string, ServerClient> clients;
+        private static TcpListener tcpListener; 
+
+        public static void Main(string[] args)
+        {
+            RunServer();
+        }
+
+        public static void RunServer()
+        {
+            clients = new Dictionary<string, ServerClient>();
+            tcpListener = new TcpListener(System.Net.IPAddress.Any, 60000);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(connectClientsToServer), null);
+
+        }
+
+        public static void connectClientsToServer(IAsyncResult ar)
+        {
+            var tcpClient = tcpListener.EndAcceptTcpClient(ar);
+            Console.WriteLine($"Added: {tcpClient.Client.RemoteEndPoint} to server");
+            string[] dataString = readData(tcpClient.GetStream());
+            clients.Add(dataString[0], new ServerClient(tcpClient.Client.RemoteEndPoint.ToString(), dataString[0], dataString[1], tcpClient));
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(connectClientsToServer), null);
+        }
+
+        public static string[] readData(NetworkStream stream)
+        {
+            byte[] buffer = new byte[4];
+            stream.Read(buffer, 0, buffer.Length);
+            int length = BitConverter.ToInt32(buffer);
+            stream.Read(buffer, 0, length);
+            string dataString = Encoding.ASCII.GetString(buffer);
+            string[] dataStringArray = dataString.Split(" ");
+
+            return dataStringArray;
+        }
+    }
+}
