7808f7aa343948832b745cd84d310db5fa8cb363
david <davidstulemeijer@gmail.com>
2021-10-05 17:27:26 +0200

    first impelementation of mainWindow
    Does not have the correct bindings yet, so uses dummy data

--- Ludavi/Ludavi Client/Views/MainWindow.xaml.cs
+++ Ludavi/Ludavi Client/Views/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Ludavi_Client
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
